3d5ee07b9bc1fd3cf8faba5765ac356f26f9dca1
GyuYeong <77821573+SimGyuYeong@users.noreply.github.com>
2021-07-24 17:05:08 +0900

    210724 - 게임 제출

--- Assets/scripts/GameStartManager.cs
+++ Assets/scripts/GameStartManager.cs
@@ -5,19 +5,16 @@ using UnityEngine.SceneManagement;
 
 public class GameStartManager : MonoBehaviour
 {
-    private UIManager ui;
     private GameOverManager over;
 
     void Start()
     {
-        ui = FindObjectOfType<UIManager>();
         over = FindObjectOfType<GameOverManager>();
     }
 
     public void GameStart()
     {
         SceneManager.LoadScene("Main");
-        ui.scoreReset();
     }
 
     public void Quit()
